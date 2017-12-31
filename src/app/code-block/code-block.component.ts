6cd95f3732759447d46c6f7040134ee1906a0446
Rajinder Yadav <devguy.ca@gmail.com>
2017-12-31 14:20:45 -0500

    Test code commented.

--- src/app/code-block/code-block.component.ts
+++ src/app/code-block/code-block.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ElementRef } from '@angular/core';
+import * as hljs from 'highlight.js';
+
+// Usage: Template
+//  <code-block [code]="html" cls="html w70 mxa"></code-block>
 
 @Component({
   selector: 'code-block',
   templateUrl: './code-block.component.html',
   styleUrls: ['./code-block.component.scss']
 })
-export class CodeBlockComponent implements OnInit {
+export class CodeBlockComponent implements OnInit, AfterViewInit {
 
   @Input() code = '';
-  @Input() cls = 'js';
-  constructor() { }
+  @Input() cls = '';
 
-  ngOnInit() {
+  constructor(private el: ElementRef) {
+    console.log('code-block ctor');
   }
 
+  ngOnInit() {
+    console.log('code-block init');
+  }
+
+  ngAfterViewInit() {
+  console.log('after view init');
+    // this.el.nativeElement.class = `${this.el.nativeElement.class} js w70 mxa`;
+    // this.el.nativeElement.innerText = this.code;
+    // hljs.highlightBlock(this.el.nativeElement);
+  }
 }
