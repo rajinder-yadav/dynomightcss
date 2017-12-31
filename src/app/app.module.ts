35d0c042824a9c0c04643c9905f8c061d3d9d07c
Rajinder Yadav <devguy.ca@gmail.com>
2017-12-31 14:19:05 -0500

    Menubar and navigation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CodeBlockComponent } from './code-block/code-block.component';
 import { MainComponent } from './main/main.component';
+import { DocumentComponent } from './document/document.component';
+import { SyntaxDirective } from './syntax.directive';
+
+import * as hljs from 'highlight.js';
+hljs.initHighlightingOnLoad();
 
 const mainRoutes: Routes = [
-  { path: '', component: MainComponent, pathMatch: 'full'}
+  { path: '', component: MainComponent, pathMatch: 'full'},
+  { path: 'document', component: DocumentComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     CodeBlockComponent,
-    MainComponent
+    MainComponent,
+    DocumentComponent,
+    SyntaxDirective
   ],
   imports: [
     RouterModule.forRoot(mainRoutes),
