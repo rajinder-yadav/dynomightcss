c4ee4a2eaf6901cd187d60b1cf281ea812f75bc8
Dev Guy <devguy.ca@gmail.com>
2023-11-30 15:24:06 -0500

    edit: add / prefix to path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
 ];
